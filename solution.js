4dfb726ba274bf8153e65a2fe0adb66007976495
quteron <quteron@gmail.com>
2016-03-05 01:21:38 -0500

    Added empty solution pattern for js challenges

---
+++ solution.js
@@ -0,0 +1,58 @@
+/*------------------------------ Scanner ------------------------------*/
+function Scanner(input) {
+  this.input = input;
+  this.tokens = input.split(/\s+/);
+  this.pos = 0;
+}
+
+Scanner.prototype.next = function next() {
+  return this.tokens[this.pos++];
+};
+
+Scanner.prototype.nextString = function nextString() {
+  return String(this.next());
+};
+
+Scanner.prototype.nextNumber = function nextNumber() {
+  return Number(this.next());
+};
+
+Scanner.prototype.hasNext = function hasNext() {
+  return this.pos !== this.tokens.length;
+};
+
+Scanner.prototype.nextArray = function nextArray() {
+  var size = this.nextNumber();
+  var array = new Array(size);
+  for (var i = 0; i < size; i++) {
+    array[i] = this.nextNumber();
+  }
+
+  return array;
+};
+
+Scanner.prototype.nextArray = function nextArray(size) {
+    var array = new Array(size);
+    for (var i = 0; i < size; i++) {
+        array[i] = this.nextNumber();
+    }
+
+    return array;
+};
+
+/*------------------------------ Read Input ------------------------------*/
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
+
+/*------------------------------ Challenge Solution ------------------------------*/
+function processData(input) {
+   var sc = new Scanner(input);
+} 
